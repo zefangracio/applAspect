fb39f44c23118ab9bfcb19b2f2805dc6f9655fc0
zefangracio <48423324+zefangracio@users.noreply.github.com>
2020-05-06 14:32:48 +0700

    bener
    bener

---
+++ appl/src/main/java/com/appl/aop/AlatMusikAspect.java
@@ -0,0 +1,36 @@
+package com.appl.aop;
+
+import com.appl.model.AlatMusik;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+
+
+@Aspect
+public class AlatMusikAspect {
+	@Pointcut("execution(** com.appl.services.AlatMusikService.create())")
+	public void show() {}
+	
+	@Before("show()")
+	public void pesan1() {
+		System.out.println("Membuat data...");
+	}
+	
+/*	@Before("show()")
+	public void takeSeats() {
+		System.out.println("Taking seats");
+	}
+*/	
+	@AfterReturning("show()")
+	public void applause() {
+		System.out.println("CLAP CLAP CLAP!!!");
+	}
+	
+	@AfterThrowing("show()")
+	public void demandRefund() {
+		System.out.println("Demanding a refund");
+	}
+}
--- appl/src/main/java/com/appl/configuration/MongoConfig.java
+++ appl/src/main/java/com/appl/configuration/MongoConfig.java
@@ -3,20 +3,23 @@ package com.appl.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
- 
+
+import com.appl.aop.AlatMusikAspect;
 import com.mongodb.MongoClient;
- 
+@EnableAspectJAutoProxy
 @Configuration
-@ComponentScan(basePackages = "com.appl")
+@ComponentScan(basePackages = "com.appl.services")
 @EnableMongoRepositories({ "com.appl.repositories" })
-public class MongoConfig {
- 
-    @Bean
-    public MongoDbFactory mongoDbFactory() throws Exception {
+public class MongoConfig extends AbstractMongoConfiguration {
+	
+	@Bean
+    public MongoDbFactory mongoDbFactory() {
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         return new SimpleMongoDbFactory(mongoClient, "alatMusik");
     }
@@ -26,5 +29,20 @@ public class MongoConfig {
         MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
         return mongoTemplate;
     }
+    
+	@Bean
+	public AlatMusikAspect alatMusikAspect() {
+		return new AlatMusikAspect();
+	}
+
+	@Override
+	public MongoClient mongoClient() {
+		return new MongoClient();
+	}
+
+	@Override
+	protected String getDatabaseName() {
+		return "appl";
+	}
  
 }
